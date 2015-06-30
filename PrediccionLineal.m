af193b7caa4dada3212544720d8e9518d7c9e6f1
robercrash <robercrash@hotmail.com>
2015-06-30 00:44:04 -0500

    Actualizaciones al calculo de LPC's
    Aún no funciona correctamente pero parece ser que los coeficientes ya
    se recorren de la manera adecuada.

--- PrediccionLineal.m
+++ PrediccionLineal.m
@@ -42,7 +42,7 @@ title('Señal después de ventaneo()');
 
 %% 3) Calculo de Autocorrelación
 r = zeros(1, noDeVentanas * p);                 % tendremos p coeficientes por cada ventana
-%yV = ones(1, 16000);
+yV = ones(1, 16000);
 
 for i=1: 1: noDeVentanas
     
@@ -62,92 +62,37 @@ end
 
 %% Calculando LPC's con algoritmo Levinson-Durbin
 
-k = zeros(1, noDeVentanas * p);                         %Vector de Coeficientes de Reflexion
-alpha = zeros (noDeVentanas * p, noDeVentanas * p);
-E = zeros(1, noDeVentanas * p); 
-a = zeros(1, noDeVentanas * p);
+E(1) = r(1);
+%Alpha = ones(10, 10);
 
-for h=1: 1: 1
+
+for i=2: 1: p+1 % lo correcto es hasta p+1         
     
-    E(1) = r(1);
-
-    for i=2: 1: p 
-
-        alphaSum = 0;
-
-        for j=1: 1: p - 1
-
-            if(i-j) <= 0
-                alphaSum = alphaSum + ( alpha(i-1, j) * 0 );
-            else
-                alphaSum = alphaSum + ( alpha(i-1, j) * r(i-j) ); 
-            end
-%             fprintf('alpha( %d, %d) * ', i-1, j);             %Verificacion del recorrido de alpha(n)
-%             fprintf('r(%d)\n', i - j);                    %Verificacion del recorrido de r(n)
-        end
-%         fprintf('\n\n', alphaSum);             %Needed for debuging para Verificacion de recorridos
-
-        k(i-1) = (r(i-1) - alphaSum) / E(i-1);
-        alpha(i-1, i-1) = k(i-1);
-        
-        for j=1: 1: i-1
-            
-            alpha(i, j) = alpha(i-1, j) - ( k(i) * alpha(i-1, i-j) );
-            
-        end
-    %     
-    %     E(i) = (1 - (k(i)^2)) * E( (i+1) - 1 );
-
+    alphaSum = 0;
+    
+    for j=1: 1: i-1
+    
+        %alphaSum = alphaSum  + ( Alpha(i-1, j) * r( (i-j) ) );
+        fprintf('Alpha(%d, %d) * r(%d) -- AQUI!\n\n', (i-1), j, (i-j));    
+    
     end
+    
+    %k(i) = ( r(i) - alphaSum ) / E(i - 1);
+    fprintf('alphaSum = %f\n', alphaSum);
+    fprintf('k(%d) = r(%d) - alphaSum / E(%d)\n\n', i, i, (i-1));
 
+    %Alpha(i, i) = k(i);
+    fprintf('Alpha(%d, %d) = k(%d)\n\n',i, i, i);
+    
+    
+    for j=1: 1: i-1
+    
+        %Alpha(i, j) = Alpha(i-1, j) - ( k(i) * Alpha(i-1, i-j) ); 
+        fprintf('Alpha(%d, %d) = Alpha(%d, %d) - ( k(%d) * Alpha(%d, %d) )\n',i , j, i-1, j, i, i-1, i-j);
+    
+    end
+    
+    %E(i) = (1 - (k(i)^2) ) * E(i-1);
+    fprintf('\nE(%d) = (1 - (k(%d))^2 ) * E(%d)\n\n\n\n', i, i, i-1);
+    
 end
-
-
-
-% a = zeros(1, noDeVentanas * p);
-% an = zeros(1, noDeVentanas * p);
-% 
-% % R[i] - autocorrelation coefficients
-% % R(i) --> r(i) 
-% 
-% % A[i] - filter coefficients
-% % A(i) --> a(i)
-% 
-% % K - reflection coefficients
-% % K --> k
-% 
-% % Alpha - prediction gain
-% % Alpha --> alpha 
-% 
-% % initialization 
-% 
-% % A[0] = 1
-% a(1) = 1;
-% %K = -R[1]/R[0]
-% k = -r(2) / r(1);
-% %A[1] = K
-% a(2) = k;
-% %Alpha = R[0]* (1-K^2)
-% alpha = r(1) * ( 1 - (k^2) );
-% 
-% % For i = 2 To M
-% % S = SUM(R[j]*A[i-j];j=1,i-1) + R[i]
-% % K = -S/Alpha
-% % An[j] = A[j] + K*A[i-j] /*for j=1 to i-1 where An[i] = new A[i]*/
-% % An[i] = K
-% % Alpha = Alpha * (1-K^2)
-% % End
-% 
-% s = 0;
-% 
-% for i=2: 1: 8
-%     
-%     for j=1: 1: i-1
-%         s = s + ( r(j) * a(i-j));  
-%         k =  -s / alpha;
-%         an(j) = a(j) + k * a(i-j);
-%         an(i) = k;
-%         alpha = alpha * (1-k^2);
-%     end
-%     
-% end
